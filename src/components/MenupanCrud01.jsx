3b9c3ddcf7aaa707a8ec3fb1a73fc07a6f74acc6
subinsong01 <haely97@naver.com>
2024-08-29 20:58:19 +0900

    feat: add router page

---
+++ src/components/MenupanCrud01.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuAdminInputPage from './MenuAdminInputPage';
+
+function MenupanCrud01(props) {
+  return (
+   <>
+   <MenuAdminInputPage />
+   </>
+  );
+}
+
+export default MenupanCrud01;
