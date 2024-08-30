ff668832fda81690b502be0bfd5260293dfbdd52
subinsong01 <haely97@naver.com>
2024-08-30 14:34:40 +0900

    design: apply tailwind css

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -6,17 +6,19 @@ function MainPage(props) {
   const navigate = useNavigate();
   return (
     <div className="md:container md:mx-auto">
-      <h1 className='font-bold'>메뉴판 관리 메인</h1>
+      <h1 className='font-bold text-xl text-center mt-4'>메뉴판 관리 메인</h1>
+      <div className='flex justify-center pt-6'>
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
         onClick={() => navigate("/")}
       >메뉴판</button>
       <button 
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
         onClick={() => navigate("/list")}>메뉴관리</button>
       <button 
       onClick={() => navigate("/manage")}
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">주문관리</button>
+      </div>
     </div>
   );
 }
