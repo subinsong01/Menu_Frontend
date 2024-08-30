9e38e58ccd9b9fb9e13bcba89f1c83576fa457d8
subinsong01 <haely97@naver.com>
2024-08-30 14:36:20 +0900

    design: apply tailwind css

--- src/components/MenuAdminInputPage.jsx
+++ src/components/MenuAdminInputPage.jsx
@@ -1,57 +1,77 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function MenuAdminInputPage(props) {
+    const navigate = useNavigate();
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
 
         const formData = new FormData(e.target);
         const data = Object.fromEntries(formData.entries());
-        //체크안하면 "N"으로 설정해줌
-        data.sayong = (data.sayong)?data.sayong:"N"
+        // 체크하지 않으면 "N"으로 설정
+        data.sayong = data.sayong ? data.sayong : "N";
         console.log(data);
 
-        let url = 'http://localhost:8080/menus'
-        fetch( url , {
-            method: 'POST', //새로운값 추가시 POST
-            body: JSON.stringify(data),//추가할객체
+        let url = 'http://localhost:8080/menus';
+        fetch(url, {
+            method: 'POST', // 새로운 값을 추가시 POST
+            body: JSON.stringify(data), // 추가할 객체
             headers: {
-              'Content-type': 'application/json; charset=UTF-8',
+                'Content-type': 'application/json; charset=UTF-8',
             },
-          })
-        .then((response) => response.json())
-        .then((json) => {
-            alert("등록완료")
-            console.log(json)
-        });
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                alert("등록완료");
+                console.log(json);
+            });
     }
 
     return (
         <div>
-            <h1>메뉴등록</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    구분 : 
-                    <select name="gubun">
-                        <option>식사</option>
-                        <option>간식</option>
-                    </select>
-                </label><br></br>
-                <label>
-                    메뉴명 : 
-                    <input name='name'></input>
-                </label><br></br>
-                <label>
-                    가격 : 
-                    <input type="number" name='price'></input>
-                </label><br></br>
-                <label>
-                    사용여부 : 
-                    <input type="checkbox" name='sayong' value="Y"></input>
-                </label><br></br>
-                <input type='submit' value="등록"></input>
-                <button>목록</button>
-            </form>
+            <h1 className='font-bold text-xl text-center mt-4'>메뉴 등록</h1>
+            <div className='flex justify-center pt-6'>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        구분:
+                        <select name="gubun">
+                            <option>식사</option>
+                            <option>간식</option>
+                        </select>
+                    </label>
+                    <br />
+                    <label className='container mx-auto'>
+                        메뉴명:
+                        <input name='name' required />
+                    </label>
+                    <br />
+                    <label>
+                        가격:
+                        <input type="number" name='price' required />
+                    </label>
+                    <br />
+                    <label>
+                        사용여부:
+                        <input type="checkbox" name='sayong' value="Y" />
+                    </label>
+                    <br />
+                    <input 
+                        type='submit' 
+                        value="등록" 
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4 mt-4" 
+                    />
+                    <button 
+                        type="button"
+                        className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => {
+                            navigate("/list");
+                        }}
+                    >
+                        목록
+                    </button>
+                </form>
+            </div>
         </div>
     );
 }
--- src/components/MenuAdminListPage.jsx
+++ src/components/MenuAdminListPage.jsx
@@ -1,53 +1,61 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function MenuAdminListPage(props) {
+function MenuAdminListPage() {
+    const [listData, setListData] = useState([]);
+    const navigate = useNavigate();
 
-    const [ listData, setListData ] = useState([])
-
-    function fetchList(){
-        let url = 'http://localhost:8080/menus'
-        fetch( url )
-        .then((response) => response.json())
-        .then((json) => {
-            console.log("fetchList : ",json)
-            setListData(json.data)
-        });
+    function fetchList() {
+        let url = 'http://localhost:8080/menus';
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                console.log("fetchList: ", json);
+                setListData(json.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching data: ", error);
+            });
     }
-    
-    useEffect(fetchList,[])
+
+    useEffect(fetchList, []);
 
     return (
-        <>
-            <h1>메뉴목록</h1>
-            <div>
-                <select name='gubun'>
+        <div>
+            <h1 className='font-bold text-xl text-center mt-4'>메뉴목록</h1>
+            <div className='flex justify-center pt-6'>
+                <select name='gubun' className='flex justify-center mr-4'>
                     <option>전체</option>
                     <option>식사</option>
                     <option>간식</option>
                 </select>
-                <button onClick={fetchList}>새로고침</button>
-                <button>메인</button>
-                <button>목록</button>
+                <div>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+                        onClick={fetchList}>새로고침</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+                        onClick={() => navigate("/")}>메인</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+                        onClick={() => navigate("/input")}>등록</button>
+                </div>
             </div>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>구분</th>
-                            <th>메뉴명</th>
-                            <th>단가</th>
-                            <th>사용</th>
-                            <th>주문수</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+            <div className='flex justify-center'>
+            <table className="border-collapse border border-slate-400 mt-6">
+                <thead>
+                    <tr>
+                        <th className="border border-slate-300 bg-sky-500/75 text-white font-bold">구분</th>
+                        <th className="border border-slate-300 bg-sky-500/75 text-white font-bold">메뉴명</th>
+                        <th className="border border-slate-300 bg-sky-500/75 text-white font-bold">단가</th>
+                        <th className="border border-slate-300 bg-sky-500/75 text-white font-bold">사용</th>
+                        <th className="border border-slate-300 bg-sky-500/75 text-white font-bold">주문수</th>
+                    </tr>
+                </thead>
+                <tbody>
                         {listData.map((dt,index)=>{
                             return (
                         <tr key={index}>
                             <td>{dt.gubun}</td>
                             <td><a href="#" onClick={(e)=>{
                                 e.preventDefault();
-                                
                             }}>{dt.name}</a></td>
                             <td>{dt.price}</td>
                             <td>{dt.sayong}</td>
@@ -58,7 +66,7 @@ function MenuAdminListPage(props) {
                     </tbody>
                 </table>
             </div>
-        </>
+        </div>
     );
 }
 
